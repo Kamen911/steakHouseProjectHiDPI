60407f85435009893ba8d5fcd5ed004018b48438
Kamen Kirilov <kamentrg91@gmail.com>
2018-10-06 11:02:41 +0300

    commit after laptop repair

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,22 @@
-(function ($) {
-   $(document).on('ready', function ()  {
+(function($) {
+  $(document).on("ready", function() {
+    // scroll indicator
+    $(function() {
+      $("a[href*=#aboutUs]").on("click", function(e) {
+        e.preventDefault();
+        $("html, body").animate(
+          {
+            scrollTop: $($(this).attr("href")).offset().top
+          },
+          500,
+          "linear"
+        );
+      });
+    });
 
-
-
-
-      // scroll indicator
-		$(function () {
-			$('a[href*=#aboutUs]').on('click', function (e) {
-				e.preventDefault();
-				$('html, body').animate({
-					scrollTop: $($(this).attr('href')).offset().top
-				}, 500, 'linear');
-			});
-		});
-
-		// toggle class on hamburgegr to overflow:hidden on body
-		$('.navbar-toggler').on('click', function () {
-			$('body').toggleClass('hamburger-on');
-		});
-
-   });
+    // toggle class on hamburgegr to overflow:hidden on body
+    $(".navbar-toggler").on("click", function() {
+      $("body").toggleClass("hamburger-on");
+    });
+  });
 })(jQuery);
